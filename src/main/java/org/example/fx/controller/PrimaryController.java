a1f9755df1f56ad46d2effb4e1e4629ad43ebc8e
prietjos <JoseM.PrietoVillar@externals.adidas.com>
2022-04-28 15:03:24 +0200

    Adding new key event.

--- src/main/java/org/example/fx/controller/PrimaryController.java
+++ src/main/java/org/example/fx/controller/PrimaryController.java
@@ -1,16 +1,22 @@
 package org.example.fx.controller;
 
+import javafx.event.Event;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import org.example.fx.App;
+import org.example.fx.controller.events.MyKeyEvent;
 import org.example.fx.controller.events.MyMouseEnteredEvent;
 import org.example.fx.controller.events.MyMouseExitedEvent;
 
@@ -18,6 +24,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Primary Controller Class.
+ * @author jose.m.prieto.villar
+ */
 public class PrimaryController implements Initializable {
 
     @FXML
@@ -27,12 +37,32 @@ public class PrimaryController implements Initializable {
     private ImageView boxImage;
 
     @FXML
+    private TextField campoTexto;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    /**
+     * switch to secondary window.
+     * @throws {@code IOException}
+     */
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
 
+//    @FXML
+//    private void printKey(KeyEvent event){
+//        System.out.println(event.getText());
+//    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //campoTexto.setOnKeyPressed(key -> System.out.println(key.getText()));
+
+        anchorPane.addEventFilter(KeyEvent.KEY_RELEASED, new MyKeyEvent());
+
         primaryButton.setText("Boton Inicial");
 
         boxImage.setOnMouseEntered(new MyMouseEnteredEvent());
